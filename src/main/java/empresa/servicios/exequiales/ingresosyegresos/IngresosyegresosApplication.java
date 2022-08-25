de344e329ddd248d14e36e66bf4c319d5f541de1
Rosita Torres Macias <rositatorresmacias@gmail.com>
2022-08-25 14:36:40 -0500

    primer commit

---
+++ src/main/java/empresa/servicios/exequiales/ingresosyegresos/IngresosyegresosApplication.java
@@ -0,0 +1,13 @@
+package empresa.servicios.exequiales.ingresosyegresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngresosyegresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngresosyegresosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/empresa/servicios/exequiales/ingresosyegresos/entity/Empresa.java
@@ -0,0 +1,70 @@
+package empresa.servicios.exequiales.ingresosyegresos.entity;
+
+public class Empresa {
+    private long idEmpresa; // variable de tipo objeto
+    private String nit;
+    private String nombreEmpresa;
+    private String direccion;
+    private String telefono;
+    private boolean estado;
+
+    public long getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(long idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "idEmpresa=" + idEmpresa +
+                ", nit='" + nit + '\'' +
+                ", nombreEmpresa='" + nombreEmpresa + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
+}
