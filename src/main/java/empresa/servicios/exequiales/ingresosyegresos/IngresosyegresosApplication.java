b49ca045a5dc46b8354105f19bf28017add4be4a
SADIDROMERO <89227037+SADIDROMERO@users.noreply.github.com>
2022-08-30 20:02:09 -0500

    Update IngresosyegresosApplication.java

--- src/main/java/empresa/servicios/exequiales/ingresosyegresos/IngresosyegresosApplication.java
+++ src/main/java/empresa/servicios/exequiales/ingresosyegresos/IngresosyegresosApplication.java
@@ -12,7 +12,8 @@ public class IngresosyegresosApplication {
 
 	public static void main(String[] args) {
 		
-		
+		SpringApplication.run(IngresosyegresosApplication.class, args);
+	}
 
 
 		Empresa Funeraria = new Empresa(1,"80098967","Servicios Excequiales","Avenida siempre viva 123","3145678900", true);
@@ -34,8 +35,7 @@ public class IngresosyegresosApplication {
 		Ingreso.setMontoMovimiento(200F);
 		System.out.println(Ingreso.getMontoMovimiento());
 
-		SpringApplication.run(IngresosyegresosApplication.class, args);
-	}
-
+	
+	
 
 }
